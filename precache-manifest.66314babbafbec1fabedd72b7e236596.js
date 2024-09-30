96702a5d32e5fb71958cf8e1658ce9214701a751
atiqsayyeddd <atiq.sayyed@doordash.com>
2024-09-30 12:54:14 +0530

    Updates

---
+++ precache-manifest.66314babbafbec1fabedd72b7e236596.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6a5dd50a767ba70688894e90e0873a6",
+    "url": "/react-landing-page-template/index.html"
+  },
+  {
+    "revision": "0a3a647e169f899bbc80",
+    "url": "/react-landing-page-template/static/css/main.431b0579.chunk.css"
+  },
+  {
+    "revision": "889c029f287e5feab6fb",
+    "url": "/react-landing-page-template/static/js/2.41457aee.chunk.js"
+  },
+  {
+    "revision": "0a3a647e169f899bbc80",
+    "url": "/react-landing-page-template/static/js/main.57c72a28.chunk.js"
+  },
+  {
+    "revision": "2d0063401c51aeddc36e",
+    "url": "/react-landing-page-template/static/js/runtime~main.78e5b170.js"
+  }
+]);
